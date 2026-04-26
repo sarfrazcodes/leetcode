9e49cf814092fc8ec62cb07ce2c44e27cf9f49d7
Mohd Sarfraz Saifi <sarfrazcodes@gmail.com>
2026-04-26 19:22:17 +0530

    Time: 9 ms (7.02%), Space: 9.6 MB (91.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        vector<int> freq(26,0);
-        for(int i=0;i<s.length();i++){
-            freq[s[i]-'a']++;
-            freq[t[i]-'a']--;
-        } 
-        for( auto it : freq){
-            if(it!=0) return false;
-        }  
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        else return false;
     }
 };
